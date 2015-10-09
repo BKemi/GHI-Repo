f27877ba6bba21e53e325b4d320024626e8b9d9d
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-09 20:51:21 +0200

    New live version

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 /*jslint browser: true*/
+/*jshint camelcase: false */
 /*global L */
+/*global $ */
 
 function getColor(d) {
   if (d === '-') { return '#808080'; }
@@ -12,15 +14,26 @@ function getColor(d) {
     '#eaeaea';
 }
 
-function getSeverity(d) {
-  if (d === '-') { return 'No data'; }
-  if (d === '<5') { return 'Low'; }
-  return d >= 50 ? 'Extremely alarming' :
-    d >= 35  ? 'Alarming' :
-    d >= 20  ? 'Serious' :
-    d >= 10  ? 'Moderate' :
-    d >= 0   ? 'Low' :
-    'Not calculated';
+function getSeverity(d, lang) {
+  if (lang === 'de') {
+    if (d === '-') { return 'Keine Angaben'; }
+    if (d === '<5') { return 'Wenig'; }
+    return d >= 50 ? 'Gravierend' :
+      d >= 35  ? 'Sehr ernst' :
+      d >= 20  ? 'Ernst' :
+      d >= 10  ? 'Mäßig' :
+      d >= 0   ? 'Wenig' :
+      'Nicht berechnet';
+  } else {
+    if (d === '-') { return 'No data'; }
+    if (d === '<5') { return 'Low'; }
+    return d >= 50 ? 'Extremely alarming' :
+      d >= 35  ? 'Alarming' :
+      d >= 20  ? 'Serious' :
+      d >= 10  ? 'Moderate' :
+      d >= 0   ? 'Low' :
+      'Not calculated';
+  }
 }
 
 function getSeverityClass(d) {
@@ -33,7 +46,20 @@ function getSeverityClass(d) {
     d >= 0   ? 'low' :
     'not-calculated';
 }
-
+var messages_en = {
+  findout: 'Find out more',
+  insuf_data: 'INSUFFICIENT DATA',
+  not_calculated: 'Not calculated',
+  score: 'Score',
+  level: 'Level'
+};
+var messages_de = {
+  findout: 'Mehr erfahren',
+  insuf_data: 'UNZUREICHENDE DATEN',
+  not_calculated: 'Nicht berechnet',
+  score: 'Punkte',
+  level: 'Wert'
+};
 
 (function (window, document, L, undefined) {
   'use strict';
@@ -43,21 +69,21 @@ function getSeverityClass(d) {
   /* create leaflet map */
   var map = L.map('map', {
     center: [32.5377, 13.3958],
+    maxBounds: [[90, 180], [-90, -180]],
     scrollWheelZoom: false,
     boxZoom: false,
-    worldCopyJump: true,
+    // worldCopyJump: true,
     zoom: 2,
     minZoom: 2,
     maxZoom: 18,
     zoomControl: false
   });
-  map.addControl(new L.Control.ZoomMin())
+  map.addControl(new L.Control.ZoomMin());
 
   new L.tileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-base/{z}/{x}/{y}.png', {
       subdomains: '0123',
       }).addTo(map);
 
-  var info = L.control();
   var geojsonLayer;
 
   function style(feature) {
@@ -95,28 +121,44 @@ function getSeverityClass(d) {
     // info.update();
   }
 
-  function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-  }
+  //function zoomToFeature(e) {
+  //  map.fitBounds(e.target.getBounds());
+  //}
+  
+
 
   function onEachFeature(feature, layer) {
-
     // set up popups
-    var popupContent;
-    if (feature.properties.score !== 'nc') {
-
-      // are we in the embed page? If so, links open in a new window
-      var url = window.location.href;
-      if (url.indexOf('embed') > -1) {
-        popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong>' + feature.properties.score + '</strong></p> <p>Level: <strong>' + getSeverity(feature.properties.score) + '</strong></p> <p><a class="button small radius" target="_blank" href="countries/' + feature.id + '">Find out more</a></p>';
-      } else {
-        popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong>' + feature.properties.score + '</strong></p> <p>Level: <strong>' + getSeverity(feature.properties.score) + '</strong></p> <p><a class="button small radius" href="countries/' + feature.id + '">Find out more</a></p>';
-      }
+    var url = window.location.href;
+    var m;
+    var level;
+    if (url.indexOf('/de') > -1) {
+      m = messages_de;
+      level = getSeverity(feature.properties.score, 'de');
     } else {
-      popupContent = '<h4>' + feature.properties.name + '</h4> <p>Score: <strong> Not calculated</p>';
+      m = messages_en;
+      level = getSeverity(feature.properties.score, 'en');
     }
 
+    var popupContent = '<h4 id=' + feature.id + '>' + feature.properties.name + '</h4>';
 
+    if (feature.properties.score === '-') {
+      popupContent += '<p><strong>' + m.insuf_data + '</strong></p>';
+    } else if (feature.properties.score === 'nc') {
+      popupContent += '<p>' + m.score + ': <strong>' + m.not_calculated + '</strong></p>';
+    } else {
+      popupContent += '<p>' + m.score + ': <strong>' + feature.properties.score + '</strong></p>';
+      popupContent += '<p>' + m.level + ': <strong>' + level + '</strong></p>';
+    }
+
+    if (feature.properties.score !== 'nc') {
+      if (url.indexOf('embed') > -1) {
+        popupContent += '<p><a class="button small radius" target="_blank" href="countries/' + feature.id + '">' + m.findout + '</a></p>';
+      } else {
+        popupContent += '<p><a class="button small radius" href="countries/' + feature.id + '">' + m.findout + '</a></p>';
+      }
+    }
+    // done with popup setup
 
     layer.bindPopup(popupContent, {autopan: true});
     // set up mouseover highlights
@@ -130,7 +172,13 @@ function getSeverityClass(d) {
   }
 
   // https://gis.stackexchange.com/a/102125
-  geojsonLayer = new L.GeoJSON.AJAX('data/countrydata-2015.geo.json', {
+  var jsonfile;
+  if (window.location.href.indexOf('/de') > -1) {
+    jsonfile = '../data/countrydata-2015.geo.json';
+  } else {
+    jsonfile = 'data/countrydata-2015.geo.json';
+  }
+  geojsonLayer = new L.GeoJSON.AJAX(jsonfile, {
     style: style,
     onEachFeature: onEachFeature
   });       
@@ -140,41 +188,60 @@ function getSeverityClass(d) {
   // https://stackoverflow.com/a/24529886
   map.on('popupopen', function(centerMarker) {
           var cM = map.project(centerMarker.popup._latlng);
-          cM.y -= centerMarker.popup._container.clientHeight/8
-          map.setView(map.unproject(cM),2, {animate: true});
+          cM.y -= centerMarker.popup._container.clientHeight/8;
+          map.setView(map.unproject(cM),map.getZoom(), {animate: true});
       });
 
   function populateTable(year) {
     // reload table
-    $.getJSON( "data/countrydata-" + year + ".geo.json", function( data ) {
-      var items = [];
+    var jsonroot;
+    if (window.location.href.indexOf('/de') > -1) {
+      jsonroot = '../data/countrydata-';
+    } else {
+      jsonroot = 'data/countrydata-';
+    }
+    $.getJSON( jsonroot + year + '.geo.json', function( data ) {
       $('#country-table tbody').empty();
       $.each( data.features, function( key, c ) {
         if (c.properties.score !== 'nc' && c.properties.score !== '-') {
-          $('<tr>').attr("id", c.id)
-            .attr("class", getSeverityClass(c.properties.score))
-            .attr("role", "row")
+          $('<tr>').attr('id', 'table-' + c.id)
+            .attr('class', getSeverityClass(c.properties.score))
+            .attr('role', 'row')
             .append(
-                $('<td>').text(c.properties.name),
-                $('<td>').text(c.properties.score)
+                $('<td class="name">').text(c.properties.name).wrapInner('<span />'),
+                $('<td class="score">').text(c.properties.score).wrapInner('<span />')
                 ).appendTo('#country-table');
         }
       });
     });
 
-    $('#country-table').on( 'click', 'tr', function (ev) {
+    $('#country-table').on( 'click', 'tr', function () {
       // clicking on a country in the table focuses the map on it
-      console.log(this);
-      var f = geojsonLayer.getLayer(this.id);
+      var f = geojsonLayer.getLayer(this.id.replace('table-', ''));
       map.setView(f.getBounds().getCenter());
       f.openPopup();
     });
+
+    map.on('popupopen', function(e) {
+      $('#table-container tr').removeClass('highlight');
+      // https://stackoverflow.com/questions/12701240/how-to-identify-leaflets-marker-during-a-popupopen-event#comment50813535_12712987
+      var country_id = e.popup._contentNode.childNodes[0].id;
+      if ($('#table-' + country_id).length) {
+        var container = $('#table-container');
+        // https://stackoverflow.com/a/2906009
+        var scroll_offset = $('#table-' + country_id).offset().top - container.offset().top + container.scrollTop();
+        $('#table-container').animate({
+          scrollTop: scroll_offset,
+        }, 300);
+        $('#table-' + country_id).addClass('highlight');
+      }
+    });
+
   }
 
-  $(document).ready(function() {
+  $(document).ready(function() {    
     populateTable('2015');
-
-    $('#year-drop li a').click( function(ev) {
+    $('#year-drop li a').click( function() {
       // year dropdown refreshes map
       var year = this.className;
       geojsonLayer.clearLayers();geojsonLayer = new L.GeoJSON.AJAX('data/countrydata-' + year + '.geo.json', {
@@ -184,15 +251,7 @@ function getSeverityClass(d) {
       geojsonLayer.addTo(map);
       $('#year-button').text(year);
       populateTable(year);
-
     });
-
-
-
   });
-
-
-
-
 }(window, document, L));
 
